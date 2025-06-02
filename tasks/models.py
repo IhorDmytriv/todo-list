9eb0cc08db745ab9e300ad263eecde45ff755104
Ihor Dmytriv <dmytriwihor@gmail.com>
2025-06-02 14:37:53 +0200

    Rename datetime field for Task model

--- tasks/models.py
+++ tasks/models.py
@@ -10,10 +10,14 @@ class Tag(models.Model):
 
 class Task(models.Model):
     content = models.CharField(max_length=100)
-    datetime = models.DateTimeField(auto_now_add=True)
+    created_time = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField(null=True, blank=True)
     is_complete = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    class Meta:
+        ordering = ['is_complete', '-created_time']
+
 
     def __str__(self):
         return f"{self.content}"
